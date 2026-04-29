94e8e0e2080acb7cfde839c72944c940deb9e309
anastttassss <anastttass@gmail.com>
2026-04-29 11:40:55 +0500

    Четвератя

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -1,14 +1,17 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
-
+namespace App\Http\Controllers\Admin;  
+use App\Models\Tag;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
+        $tag = new Tag();
+        $tag->title = 'Primer mipl';
+        $tag->save();
+
         return view('admin.index');
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use App\Models\Tag; 
 
-class Post extends Model
+class Category extends Model
 {
     use HasFactory;
+    use Sluggable;
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
+
